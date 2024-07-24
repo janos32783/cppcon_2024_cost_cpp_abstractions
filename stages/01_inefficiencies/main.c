fdace684e63453a295d1201b0084c5ab2a0530fb
Marcell Juhasz <marcell.juhasz96@gmail.com>
2024-07-24 21:30:52 +0200

    static work in progress

--- stages/01_inefficiencies/main.c
+++ stages/01_inefficiencies/main.c
@@ -12,6 +12,7 @@
 #define OSPEEDR_MASK (0x3UL)
 /* ... */
 #define GPIO_SPEED_FREQ_LOW (0x00000000U)
+#define GPIO_SPEED_FREQ_FAST (0x00000001U)
 /* ... */
 #define GPIO_MODE        (0x00000003U)
 #define GPIO_OUTPUT_TYPE (0x00000010U)
@@ -23,11 +24,23 @@ typedef enum {
     GPIO_MODE_OUTPUT = 0x00000001U
 } GPIO_Modes;
 
+typedef enum {
+    GPIO_TYPE_PP = 0x00000000U,
+    GPIO_TYPE_OD = 0x00000001U
+} GPIO_Output_Types;
+
+typedef enum {
+    GPIO_SPEED_SLOW = GPIO_SPEED_FREQ_LOW,
+    GPIO_SPEED_FAST = GPIO_SPEED_FREQ_FAST
+} GPIO_Output_Speeds;
+
 #define IS_GPIO_MODE(X) (((X) == GPIO_MODE_INPUT) || ((X) == GPIO_MODE_OUTPUT))
 
 typedef struct {
     uint32_t pin;
     GPIO_Modes mode;
+    GPIO_Output_Types type;
+    GPIO_Output_Speeds speed;
 } GPIO_InitStruct;
 
 //static inline
@@ -42,12 +55,12 @@ void GPIO_Init(GPIO_InitStruct* conf) {
     if (conf->mode == GPIO_MODE_OUTPUT) {
         temp = OSPEEDR;
         temp &= ~(OSPEEDR_MASK << (conf->pin * 2u));
-        temp |= (GPIO_SPEED_FREQ_LOW << (conf->pin * 2u));
+        temp |= (conf->speed << (conf->pin * 2u));
         OSPEEDR = temp;
 
         temp = OTYPER;
         temp &= ~(OTYPER_MASK << conf->pin);
-        temp |= (((conf->mode & GPIO_OUTPUT_TYPE) >> 4u) << conf->pin);
+        temp |= (((conf->type & GPIO_OUTPUT_TYPE) >> 4u) << conf->pin);
         OTYPER = temp;
     }
 
@@ -64,6 +77,8 @@ int main (void) {
     
     conf.pin = GPIO_PIN_6;
     conf.mode = GPIO_MODE_OUTPUT;
+    conf.type = GPIO_TYPE_OD;
+    conf.speed = GPIO_SPEED_SLOW;
 
     GPIO_Init(&conf);
     
--- stages/02_encapsulation/static/main.cpp
+++ stages/02_encapsulation/static/main.cpp
@@ -2,88 +2,125 @@
 #include <concepts>
 #include <type_traits>
 
-constexpr std::uint32_t ModeRegisterAddress = 0x48000000;
+#include "register.hpp"
 
-enum class gpio_modes : std::uint32_t {
-    input  = 0b00,
-    output = 0b01,
-    analog = 0b10
-};
+#define MODER (*((volatile uint32_t*)0x48000000))
+#define OTYPER (*((volatile uint32_t*)0x48000004))
+#define OSPEEDR (*((volatile uint32_t*)0x48000008))
+/* ... */
+#define GPIO_PIN_6 ((uint16_t)0x0006U)
+#define GPIO_PIN_7 ((uint16_t)0x0007U)
+/* ... */
+#define MODER_MASK   (0x3UL)
+#define OTYPER_MASK  (0x1UL)
+#define OSPEEDR_MASK (0x3UL)
+/* ... */
+#define GPIO_SPEED_FREQ_LOW (0x00000000U)
+#define GPIO_SPEED_FREQ_FAST (0x00000001U)
+/* ... */
+#define GPIO_MODE        (0x00000003U)
+#define GPIO_OUTPUT_TYPE (0x00000010U)
+/* ... */
+#define IS_GPIO_PIN(X) ((X) < 16 && (X) >= 0)
+#define IS_GPIO_MODE(X) (((X) == GPIO_MODE_INPUT) || ((X) == GPIO_MODE_OUTPUT))
 
-enum class pin_numbers : std::uint32_t {
-    pin_0 = 0,
-    pin_1 = 1,
-    pin_2 = 2,
-    pin_3 = 3,
-    pin_4 = 4,
-    pin_5 = 5,
-    pin_6 = 6,
-    pin_7 = 7
-};
+typedef enum {
+    GPIO_MODE_INPUT = 0x00000000U,
+    GPIO_MODE_OUTPUT = 0x00000001U
+} GPIO_Modes;
 
-class CRegister {
-private:
-    const std::uint32_t m_address;
-public:
-    CRegister (std::uint32_t address) : m_address(address) { }
+typedef enum {
+    GPIO_TYPE_PP = 0x00000000U,
+    GPIO_TYPE_OD = 0x00000001U
+} GPIO_Output_Types;
 
-    void set (std::uint32_t val) const {
-        *(reinterpret_cast<volatile std::uint32_t *>(this->m_address)) = val;
-    }
-
-    std::uint32_t get () const {
-        return *(reinterpret_cast<volatile std::uint32_t *>(this->m_address));
-    }
-
-    void set (std::uint32_t value, std::uint32_t bitmask) const {
-        std::uint32_t tmp = this->get();
-        tmp &= ~bitmask;
-        tmp |= value;
-        this->set(tmp);
-    }
-
-    void set_bits (std::uint32_t bitmask) {
-        *(reinterpret_cast<volatile std::uint32_t *>(this->m_address)) = *(reinterpret_cast<volatile std::uint32_t *>(this->m_address)) | bitmask;
-    }
-
-    void clear_bits (std::uint32_t bitmask) {
-        *(reinterpret_cast<volatile std::uint32_t *>(this->m_address)) = *(reinterpret_cast<volatile std::uint32_t *>(this->m_address)) & ~bitmask;
-    }
-
-    bool is_set (std::uint32_t bitmask) {
-        return (*(reinterpret_cast<volatile std::uint32_t *>(this->m_address)) & bitmask) == bitmask;
-    }
-
-    bool is_cleared (std::uint32_t bitmask) {
-        return (*(reinterpret_cast<volatile std::uint32_t *>(this->m_address)) & bitmask) == 0;
-    }
-
-    std::uint32_t get_bits (std::uint32_t bitmask) {
-        return *(reinterpret_cast<volatile std::uint32_t *>(this->m_address)) & bitmask;
-    }
-};
+typedef enum {
+    GPIO_SPEED_SLOW = GPIO_SPEED_FREQ_LOW,
+    GPIO_SPEED_FAST = GPIO_SPEED_FREQ_FAST
+} GPIO_Output_Speeds;
 
 class CModeRegister {
 private:
-    static inline const CRegister m_register { ModeRegisterAddress };
+    static inline const CRegister m_register { 0x48000000 };
 
-    static inline std::uint32_t calculate_value (pin_numbers pin, gpio_modes mode) {
-        return static_cast<std::uint32_t>(mode) << static_cast<std::uint32_t>(pin) * 2;
+    static inline std::uint32_t calculate_value (std::uint32_t pin, GPIO_Modes mode) {
+        return (mode & GPIO_MODE) << (pin * 2);
     }
 
-    static inline std::uint32_t calculate_bitmask (pin_numbers pin) {
-        return static_cast<std::uint32_t>(0b11) << static_cast<std::uint32_t>(pin) * 2;
+    static inline std::uint32_t calculate_bitmask (std::uint32_t pin) {
+        return MODER_MASK << (pin * 2);
     }
 public:
-    static inline void set_mode (pin_numbers pin, gpio_modes mode) {
+    static inline void set_mode (std::uint32_t pin, GPIO_Modes mode) {
         m_register.set(calculate_value(pin, mode), calculate_bitmask(pin));
     }
 };
 
+class COutputTypeRegister {
+private:
+    static inline const CRegister m_register { 0x48000004 };
+
+    static inline std::uint32_t calculate_value (std::uint32_t pin, GPIO_Output_Types type) {
+        return ((type & GPIO_OUTPUT_TYPE) >> 4u) << pin * 2;
+    }
+
+    static inline std::uint32_t calculate_bitmask (std::uint32_t pin) {
+        return OTYPER_MASK << pin;
+    }
+public:
+    static inline void set_type (std::uint32_t pin, GPIO_Output_Types type) {
+        m_register.set(calculate_value(pin, type), calculate_bitmask(pin));
+    }
+};
+
+class COutputSpeedRegister {
+private:
+    static inline const CRegister m_register { 0x48000008 };
+
+    static inline std::uint32_t calculate_value (std::uint32_t pin, GPIO_Output_Speeds speed) {
+        return speed << (pin * 2);
+    }
+
+    static inline std::uint32_t calculate_bitmask (std::uint32_t pin) {
+        return OSPEEDR_MASK << (pin * 2);
+    }
+public:
+    static inline void set_speed (std::uint32_t pin, GPIO_Output_Speeds speed) {
+        m_register.set(calculate_value(pin, speed), calculate_bitmask(pin));
+    }
+};
+
+struct GPIO_InitStruct {
+    std::uint32_t pin;
+    GPIO_Modes mode;
+    GPIO_Output_Types type;
+    GPIO_Output_Speeds speed;
+};
+
+void GPIO_Init(GPIO_InitStruct* conf) {
+    /* check the values */
+    if (!IS_GPIO_PIN(conf->pin)) { return; }
+    if (!IS_GPIO_MODE(conf->mode)) { return; }
+
+    /* configure the GPIO based on the settings */
+    if (conf->mode == GPIO_MODE_OUTPUT) {
+        COutputSpeedRegister::set_speed(conf->pin, conf->speed);
+        COutputTypeRegister::set_type(conf->pin, conf->type);
+    }
+    CModeRegister::set_mode(conf->pin, conf->mode);
+
+    /* ... */
+}
+
 int main (void) {
-    CModeRegister::set_mode(pin_numbers::pin_6, gpio_modes::output);
-    CModeRegister::set_mode(pin_numbers::pin_7, gpio_modes::input);
-    CModeRegister::set_mode(pin_numbers::pin_2, gpio_modes::analog);
+    GPIO_InitStruct conf = {0};
+    
+    conf.pin = GPIO_PIN_6;
+    conf.mode = GPIO_MODE_OUTPUT;
+    conf.type = GPIO_TYPE_OD;
+    conf.speed = GPIO_SPEED_SLOW;
+
+    GPIO_Init(&conf);
 
     while (true) { /* ... */ }
 }
