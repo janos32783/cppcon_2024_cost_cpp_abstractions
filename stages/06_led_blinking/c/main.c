be945dc572e710d3034c92314b4a5f9d5c2d0c17
Marcell Juhasz <marcell.juhasz96@gmail.com>
2024-07-31 20:55:11 +0200

    updated LED blinking implementation

--- stages/06_led_blinking/c/main.c
+++ stages/06_led_blinking/c/main.c
@@ -2,8 +2,8 @@
 #include <stdbool.h>
 
 #define AHBENR       (*((volatile uint32_t*)0x40021014))
-#define GPIO_A_MODER (*((volatile uint32_t*)0x48000000))
-#define GPIO_A_BSRR  (*((volatile uint32_t*)0x48000018))
+#define GPIO_A_MODER (*((volatile uint32_t*)0x48000800))
+#define GPIO_A_BSRR  (*((volatile uint32_t*)0x48000818))
 
 void delay (int cycles) {
     volatile int i;
@@ -14,23 +14,23 @@ int main (void) {
     uint32_t tmp;
     bool led_on = false;
 
-    AHBENR = AHBENR | 0x00020000;
+    AHBENR = AHBENR | 0x00080000;
     
     tmp = GPIO_A_MODER;
-    tmp &= ~0x00003000;
-    tmp |= 0x00001000;
+    tmp &= ~0x0c000000;
+    tmp |= 0x04000000;
     GPIO_A_MODER = tmp;
 
-    GPIO_A_BSRR = GPIO_A_BSRR | 0x00400000;
+    GPIO_A_BSRR = GPIO_A_BSRR | 0x20000000;
 
 
     while (1) {
         if (led_on) {
-            GPIO_A_BSRR = GPIO_A_BSRR | 0x00400000;
+            GPIO_A_BSRR = GPIO_A_BSRR | 0x20000000;
             led_on = false;
         }
         else {
-            GPIO_A_BSRR = GPIO_A_BSRR | 0x00000040;
+            GPIO_A_BSRR = GPIO_A_BSRR | 0x00002000;
             led_on = true;
         }
         delay(500000);
